202a46d07120ea38d78c3a0d167680831667da5a
AdyatamaMH <114073452+AdyatamaMH@users.noreply.github.com>
2022-12-18 20:57:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Add your answer here
+
+$('.lesson').hover(
+    function() {
+        $(this).find('.text-contents').css('display', 'inline-block');
+    },
+    function() {
+        $(this).find('.text-contents').css('display', 'none');
+    },
+    function() {
+        $(this).find('.text-contents').css('display', 'inline');
+    }
+);
